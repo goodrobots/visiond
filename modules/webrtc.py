4f0eb33b16c4bf886d5820b21c24d3b244c49f2f
fnoop <fnoopdogg@gmail.com>
2020-02-10 20:36:53 +0000

    Add initial webrtc module #7

---
+++ modules/webrtc.py
@@ -0,0 +1,9 @@
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+gi.require_version('GstWebRTC', '1.0')
+from gi.repository import GstWebRTC
+gi.require_version('GstSdp', '1.0')
+from gi.repository import GstSdp
+
+
