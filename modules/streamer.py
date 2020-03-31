813a8ae97f362eb7e98a682d00696972f6c57f95
fnoop <fnoopdogg@gmail.com>
2020-03-31 15:22:57 +0100

    Fix v4l2src input

--- modules/streamer.py
+++ modules/streamer.py
@@ -45,7 +45,7 @@ class Streamer(object):
         # Start with creating a pipeline from source element
         if input == "appsrc":
             self.input_appsrc()
-        elif input == "v4l2":
+        elif input == "v4l2src":
             self.input_v4l2()
         elif input == "nvarguscamerasrc":
             self.input_tegra()
